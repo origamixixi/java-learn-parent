74f74cfb15df0e7a7cc1dab91b8415618127a713
origamixixi <>
2024-09-03 09:11:57 +0800

    ioc

--- spring-learn/spring-framework-learn/src/main/java/org/example/ioc/xmlbean/XmlBeanMain.java
+++ spring-learn/spring-framework-learn/src/main/java/org/example/ioc/xmlbean/XmlBeanMain.java
@@ -9,23 +9,18 @@ public class XmlBeanMain {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring-xmlbean.xml");
         NoArgsConstructorBean noArgsConstructorBean = context.getBean(NoArgsConstructorBean.class);
-        log.info(noArgsConstructorBean.sayHello());
+        log.info("无参构造, {}", noArgsConstructorBean.sayHello());
 
         ArgsConstructorBean argsConstructorBean = context.getBean(ArgsConstructorBean.class);
-        log.info(argsConstructorBean.sayHello());
+        log.info("有参构造, {}", argsConstructorBean.sayHello());
 
         StaticFactory.StaticFactoryBean staticFactoryBean = context.getBean(StaticFactory.StaticFactoryBean.class);
-        log.info(staticFactoryBean.sayHello());
+        log.info("静态工厂, {}", argsConstructorBean.sayHello());
 
         DynamicFactory.DynamicFactoryBean dynamicFactoryBean = context.getBean(DynamicFactory.DynamicFactoryBean.class);
-        log.info(dynamicFactoryBean.sayHello());
+        log.info("动态工厂, {}", dynamicFactoryBean.sayHello());
 
         ExampleFactoryBean.ExampleBean exampleBean = context.getBean(ExampleFactoryBean.ExampleBean.class);
-        log.info(exampleBean.sayHello());
-
-        String[] beanNamesForType = context.getBeanNamesForType(StaticFactory.StaticFactoryBean.class);
-        for (String beanName : beanNamesForType) {
-            log.info("beanName: {}", beanName);
-        }
+        log.info("工厂bean, {}", exampleBean.sayHello());
     }
 }
